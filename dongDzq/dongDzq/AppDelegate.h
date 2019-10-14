6c52729b8d4bb35b82a3bd241e31c4bad72efbff
董 <dongdzq@gmail.com>
2019-10-14 08:54:04 +0800

    首次提交

---
+++ dongDzq/dongDzq/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  dongDzq
+//
+//  Created by 董卓琼 on 2019/10/12.
+//  Copyright © 2019 董卓琼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ dongDzq/dongDzq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dongDzq
+//
+//  Created by 董卓琼 on 2019/10/12.
+//  Copyright © 2019 董卓琼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
